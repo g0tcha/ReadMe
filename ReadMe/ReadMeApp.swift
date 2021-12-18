498e580ff8ee8f0015c7184e82798cb044df0940
Vincent Grossier <vincent.grossier@gmail.com>
2021-12-18 18:23:21 +0100

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Vincent Grossier on 18/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
